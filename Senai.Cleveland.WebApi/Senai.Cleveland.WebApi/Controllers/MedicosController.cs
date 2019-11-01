3270489cb6486519fa5792d4e06acc2e85ece347
Roxez <48352702+Roxez@users.noreply.github.com>
2019-11-01 10:59:11 -0300

    uppp

---
+++ Senai.Cleveland.WebApi/Senai.Cleveland.WebApi/Controllers/MedicosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Cleveland.WebApi.Repository;
+
+namespace Senai.Cleveland.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicosController : ControllerBase
+    {
+        MedicoRepositorio MedicoRepositorio = new MedicoRepositorio();
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(MedicoRepositorio.Listar());
+        }
+    }
+}
